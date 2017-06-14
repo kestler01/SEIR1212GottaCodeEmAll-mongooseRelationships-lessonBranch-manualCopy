e3190a42bdc822b2a9ab7442aa40c0d23d07515b
jordanallain <jallain@gmail.com>
2017-06-14 15:38:59 -0400

    Update master from node-template
    Refresh for cohort 019

--- bin/add-code-block.js
+++ bin/add-code-block.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
 'use strict'
+
 const readline = require('readline')
 const fs = require('fs')
 const path = require('path')
 
-const outfile = process.stdout
+const addCodeBlocks = function (inPath, outPath) {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(inPath)
+  })
 
-const rl = readline.createInterface({
-  input: fs.createReadStream(process.argv[2])
-})
+  const outStream = fs.createWriteStream(outPath)
 
-let inCodeBlock = false
+  let inCodeBlock = false
 
-rl.on('line', (line) => {
-  if (inCodeBlock) {
-    inCodeBlock = !/<!-- end code block -->/.test(line)
-    if (!inCodeBlock) outfile.write('```\n' + `${line}\n`)
-  } else {
-    outfile.write(`${line}\n`)
-    inCodeBlock = /<!-- start code block .+ -->/.test(line)
+  rl.on('line', (line) => {
     if (inCodeBlock) {
-      const filename = line.match(/ +file="(.+)" +/)[1]
-      const extension = path.extname(filename).slice(1)
-      outfile.write('```' + `${extension}\n`)
-      outfile.write(fs.readFileSync(filename))
+      inCodeBlock = !/<!-- end code block -->/.test(line)
+      if (!inCodeBlock) outStream.write('```\n' + `${line}\n`)
+    } else {
+      outStream.write(`${line}\n`)
+      inCodeBlock = /<!-- start code block .+ -->/.test(line)
+      if (inCodeBlock) {
+        const filename = line.match(/ +file="(.+)" +/)[1]
+        const extension = path.extname(filename).slice(1)
+        outStream.write('```' + `${extension}\n`)
+        outStream.write(fs.readFileSync(filename))
+      }
     }
-  }
-})
+  })
+}
+
+addCodeBlocks(process.argv[2], '/dev/stdout')
